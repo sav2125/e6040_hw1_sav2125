08f4e94577255fff0311908c65dac3e7b2869432
Ubuntu <ubuntu@ip-172-31-51-71.ec2.internal>
2016-02-15 05:48:41 +0000

    pushed hw1b.py

--- hw1b.py
+++ hw1b.py
@@ -4,9 +4,10 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-
+import glob
+import math
 from PIL import Image
-
+from theano import shared
 import theano
 import theano.tensor as T
 from theano.tensor.nnet.neighbours import images2neibs
@@ -69,7 +70,21 @@ def plot_top_16(D, sz, imname):
     imname: string
         name of file where image will be saved.
     '''
-    raise NotImplementedError
+    print ("sz shape : ", sz)
+    print ("D shape : ", D.shape)
+    f, axarr = plt.subplots(4, 4)
+
+    for i in range(4):
+        for j in range(4):
+            Dij = D[:, (i*4)+j]
+            #print ("Dij shape : ",Dij.shape)
+            d = np.reshape(Dij , (sz,sz) )
+            #plot(cij, Dij, n_blocks, X_mn, axarr[i, j])
+            ax = axarr[i,j]
+            ax.imshow(d , cmap=cm.Greys_r)
+
+    f.savefig(imname.format(sz,sz))
+    plt.close(f)
 
 
 def plot(c, D, X_mn, ax):
@@ -91,7 +106,16 @@ def plot(c, D, X_mn, ax):
 
         ax: the axis on which the image will be plotted
     '''
-    raise NotImplementedError
+    #print ("c shape : ",c.shape.eval())
+    #print ("D shape : ",D.shape.eval())
+    
+    X = np.dot(c.T,D.T)
+    print X_mn.shape
+    print X.shape
+    #X = X + np.repeat(X_mn.reshape(1, -1), X.shape[0], 0
+    X = X.reshape((256,256))
+    X = X + X_mn 
+    ax.imshow(X, cmap=cm.Greys_r)
 
 
 if __name__ == '__main__':
@@ -100,14 +124,77 @@ if __name__ == '__main__':
     to get an numpy array Ims with size (no_images, height*width).
     Make sure to sort the filenames before reading the images
     '''
+    file_list = glob.glob("./jaffe" + '/*.tiff');
+    file_list.sort()
+    LENGTH = len(file_list)
+    Ims = np.empty( (LENGTH,256,256) )
+    count_image = 0;
+    for np_name in file_list:
+        Ims[count_image] = (Image.open(np_name))
+        count_image = count_image + 1;
 
-    Ims = I.astype(np.float32)
-    X_mn = np.mean(Ims, 0)
+    Ims = Ims.reshape((LENGTH,65536))
+    Ims = Ims.astype(np.float32) 
+    X_mn = np.mean(Ims, 0)   
     X = Ims - np.repeat(X_mn.reshape(1, -1), Ims.shape[0], 0)
+    print ("X shape : ", X.shape)
+    
+    N = 16;
+    grad_old = 0;
+    Steps = 10;
+    eta = 0.01;
+    d = np.random.rand(X.shape[1],N)
+    norm_d = np.linalg.norm(d)
+    d = d/norm_d
+    D = shared(d,borrow=True)
+    lamda = shared(np.zeros((1,N)), broadcastable = (True,False))
+    print ("entered while")
+    
+    for i in range(N):
+        Di = D[ : , i]
+        prod = T.dot(T.dot(T.dot( Di.T , X.T),X),Di) 
+        
+        sum_var = T.dot(T.dot(T.dot(Di.T , D*lamda) , D.T ), Di)
+        cost = (prod - sum_var).norm(2)
+        grad = T.grad(cost, Di)
+        
+        y = Di - (eta * grad)
+        y=y/y.norm(2)
+        update_D = T.set_subtensor(Di, y)
+        f=theano.function([],y,updates=[(D,update_D)])
+        t = 1;
+        threshold = math.pow(1,-13)
+        change = 10
+        old_cost = math.pow(1,10)
+        while (t <= Steps and change > threshold):
+            
+            t = t + 1
+            z = f()
+            
+            change = np.absolute((old_cost - z).sum())
+            
+
+            old_cost = z
+        lamdai=lamda[:,i]
+        comput = T.dot(X,Di)
+        update_lamda=T.set_subtensor(lamdai,T.dot(comput.T , comput))
+        g=theano.function([],updates=[(lamda,update_lamda)])
+        g()
+        #print ("exit while")
+        print t
+        print lamdai.eval()
+        #print D.get_value()
+    #print ("X shape : ", X.shape)
+    #print ("D shape : ", D.shape.eval())
+    c = T.dot(X,D)
+    c = c.T
 
     '''
     Use theano to perform gradient descent to get top 16 PCA components of X
     Put them into a matrix D with decreasing order of eigenvalues
+    
+    
+            
 
     If you are not using the provided AMI and get an error "Cannot construct a ufunc with more than 32 operands" :
     You need to perform a patch to theano from this pull(https://github.com/Theano/Theano/pull/3532)
@@ -115,8 +202,7 @@ if __name__ == '__main__':
     '''
 
     for i in range(0,200,10):
-        plot_mul(c, D, i, X_mn.reshape((256, 256)),
-                 [1, 2, 4, 6, 8, 10, 12, 14, 16], [1, 1])
+        plot_mul(c.eval(), D.eval(), i, X_mn.reshape((256, 256)),
+                 [1, 2, 4, 6, 8, 10, 12, 14, 16])
 
-    plot_top_16(D, 256, 'output/hw1b_top16_256.png')
-
+    plot_top_16(D.eval(), 256, 'output/hw1b_top16_256.png')
